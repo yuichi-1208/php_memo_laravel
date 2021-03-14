cd38e4217472f34f13e4c862cc3205dc1dc1405e
yuichi oyama <yuichi19991208@gmail.com>
2021-03-15 00:47:41 +0900

    静的ファイルの配置と実装

--- simple_memo_laravel/public/js/app.js
+++ simple_memo_laravel/public/js/app.js
@@ -49957,8 +49957,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/oyamayuichi/workspace/docker_simple_memo_laravel/simple_memo_laravel/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/oyamayuichi/workspace/docker_simple_memo_laravel/simple_memo_laravel/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /var/www/html/simple_memo_laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/html/simple_memo_laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
