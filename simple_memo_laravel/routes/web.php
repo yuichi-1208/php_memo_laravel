67efcc7a0ff523f9e817ca8174402b38182e324a
yuichi oyama <yuichi19991208@gmail.com>
2021-03-15 00:20:48 +0900

    ルーティングの作成

--- simple_memo_laravel/routes/web.php
+++ simple_memo_laravel/routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
+Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
+Route::get('/memo', function() {
+    return view("memo");
+})->name('memo.index');
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
