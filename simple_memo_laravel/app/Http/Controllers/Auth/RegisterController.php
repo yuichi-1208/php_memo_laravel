0205d6e860bd39d6e789aebe23fc29dafa0c6033
yuichi oyama <yuichi19991208@gmail.com>
2021-03-21 01:19:16 +0900

    ログイン機能を作成

--- simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
+++ simple_memo_laravel/app/Http/Controllers/Auth/RegisterController.php
@@ -52,12 +52,13 @@ class RegisterController extends Controller
     public function register(UserCreateRequest $request)
     {
 
-        User::create([
+        $user = User::create([
             'name' =>  $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
+        $this->guard()->login($user);
         return redirect()->route('memo.index');
     }
 
--- simple_memo_laravel/routes/web.php
+++ simple_memo_laravel/routes/web.php
@@ -16,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'App\Http\Controllers\Auth\LoginController@showLoginForm')->name('login.index');
 Route::get('/user', 'App\Http\Controllers\Auth\RegisterController@showRegistrationForm')->name('user.register');
 Route::post('/user/register', 'App\Http\Controllers\Auth\RegisterController@register')->name('user.exec.register');
-Route::get('/memo', function() {
-    return view("memo");
-})->name('memo.index');
+Route::group(['middleware' => ['auth']], function() {
+    Route::get('/memo', function() {
+        return view("memo");
+    })->name('memo.index');
+});
 
 Auth::routes();
